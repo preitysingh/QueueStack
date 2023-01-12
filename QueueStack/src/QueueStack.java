2305c8cf94467f5b2ca0a1086594885a1a8a0226
lillyphan <lphan24@kentdenver.org>
2023-01-12 14:25:17 -0700

    formatting

--- QueueStack/src/QueueStack.java
+++ QueueStack/src/QueueStack.java
@@ -2,10 +2,12 @@ public class QueueStack<E> {
     private Stack<E> in;
     private Stack<E> out;
     private int size = 0;
+
     public QueueStack() {
         in = new Stack<>();
         out = new Stack<>();
     }
+
     public int size() {
             return size;
         }
