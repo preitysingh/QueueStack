7085379c16cf47c5a4c40cb4a794ecbacf97bcec
lillyphan <lphan24@kentdenver.org>
2023-01-12 14:10:36 -0700

    fix formatting

--- QueueStack/src/QueueStack.java
+++ QueueStack/src/QueueStack.java
@@ -2,29 +2,25 @@
 public class QueueStack<E> {
     private Stack<E> in;
     private Stack<E> out;
+    private int size = 0;
 
-        private Stack<E> in;
-        private Stack<E> out;
-        private int size = 0;
+    public QueueStack() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
 
-        public QueueStack() {
-            in = new Stack<>();
-            out = new Stack<>();
-        }
-
-        public int size() {
+    public int size() {
             return size;
         }
 
-        public boolean isEmpty() {
+    public boolean isEmpty() {
             return(in.isEmpty() && out.isEmpty());
         }
 
-        //E enqueue
-        //E dequeue
-        //E peek
+    //E enqueue
+    //E dequeue
+    //E peek
+    //toString
 
-        //toString
-
-    }
+}
 
